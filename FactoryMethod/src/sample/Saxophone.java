aa96ad66fe5bc0a0aee0f7a9613214e341f41d43
kbc11a08 <kbc11a08@kbc11a08-PC>
2013-11-12 11:15:06 +0900

    2013/11/12 11:14 FactoryMethodに追加

---
+++ FactoryMethod/src/sample/Saxophone.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Saxophone extends WindInstrumentProduct {
+
+	@Override
+	public void play() {
+		System.out.println("Saxophoneを演奏する　ぷぉーん");
+	}
+}
---
+++ FactoryMethod/src/sample/Trumpet.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Trumpet extends WindInstrumentProduct{
+
+	@Override
+	public void play() {
+		System.out.println("トランペットを演奏する　ぱぱぱー");
+	}
+
+}
